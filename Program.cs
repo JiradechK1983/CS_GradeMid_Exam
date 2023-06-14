d2df143922c0a5e2c1c19fdf21c279932847b1ba
Jiradej Kerdsiri <jiradech.ke@sbac.ac.th>
2023-06-14 21:11:25 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csGradeMid_Ex {
+    internal class Program {
+        static void Main(string[] args)
+        {   // Answer in 1. (1 point) 
+            
+            float stuSubjectGPAX = 0.0f;
+
+            // Answer in 2. (1 point)
+
+
+
+            // Answer in 3. (1 point)
+
+
+
+            // Answer in 4. (1 point)
+
+
+
+            // Answer in 5. (1 point)
+            for ( ; ; )
+            {
+                // Answer in 6. (1 point)
+                
+
+
+                // Answer in 7. (2 point)
+                
+
+
+                // Answer in 8. (2 point)
+                
+
+
+                // Answer in 9. (1 point)
+                
+
+
+                // Answer in 10. (2 point)
+                
+
+            }
+
+            // Answer in 11. & 12. (2 point)
+            
+            for( ; ; )
+               
+
+            //  Answer in 13. (1 point)
+            float _gradesTotal = 0.0f; 
+            foreach( in )
+                
+
+            
+
+            // Answer in 14. (1 point)
+            
+
+            Console.ReadKey();
+        }
+    }
+}
